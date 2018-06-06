e042086f626f055e5d70ae597859b032afe4d7d3
AFine970 <1049905106@qq.com>
2018-06-06 12:14:26 +0800

    最简单的爬虫

---
+++ main_spider.py
@@ -0,0 +1,27 @@
+#coding:utf-8
+#简单的豆瓣爬虫程序，参考csdn:https://blog.csdn.net/csqazwsxedc/article/details/68498842
+#Python2.7
+import requests
+from lxml import html
+
+url='https://movie.douban.com/' #this is the target
+page=requests.session().get(url)
+tree=html.fromstring(page.text)
+
+#<td class="title">
+# <a onclick="moreurl(this, {from:'mv_rk'})" href="https://movie.douban.com/subject/4920389/">头号玩家</a></td>
+
+result=tree.xpath('//td[@class="title"]//a/text()')#根据想要的内容，建议正确的正则公式
+
+
+a=[]
+a=result
+
+for b in a:
+    print b.encode('utf-8')##默认是ascII，不这样写中文可能会乱码
+
+#输出获得的数据到一个TXT
+fout=open('result.txt','w')
+for b in a:
+    fout.write('%s' % b.encode('utf-8'))
+fout.close()
